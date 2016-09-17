a5cab07c5c2b757d2c6add78bd9d90ab6f561701
Gijoe614 <jvandoorn@students.mchenry.edu>
2016-09-17 13:31:30 -0500

    Add files via upload

---
+++ mad_libs revised.py
@@ -0,0 +1,31 @@
+# Variables
+person = raw_input("please enter a persons name:")
+pet = raw_input("a type of pet:")
+color = raw_input("a color:")
+example = raw_input("example of that color:")
+place = raw_input("a place:")
+group = raw_input("a group:")
+person2 = raw_input("another person:")
+adverb = raw_input("an adverb:")
+action = raw_input("an action:")
+
+# poem
+print(person+ " had a little " + pet + ",")
+print("His fleece was " + color + " as " + example + ",")
+print("And everywhere that " + person + " went,")
+print("The " + pet + " was sure to go.\n")
+
+print("He followed him to " + place + " one day,")
+print("Which was against the rule,")
+print("It made the " + group + " laugh and play")
+print("to see a " + pet + " at the " + place + ".\n")
+
+print("And so  " + person2 + " turned it out,")
+print("But still it lingered near,")
+print("And waited " + adverb + " about,")
+print("Till " + person + " did appear.\n")
+
+print("Why does the " + pet + " love " + person + " so?")
+print("The eager " + group + " cry.")
+print("Why, " + person +" " + action + " the " + pet + ", you know,")
+print("The " + person2 + " did reply.")
